c87c65f98e09781060ed82af9fb82619afe7049d
JanPotat0 <jancquimpo@gmail.com>
2025-02-03 12:59:39 -0500

    commit - 2/3

--- ClassProjects/Project1/Main.java
+++ ClassProjects/Project1/Main.java
@@ -1,13 +1,31 @@
 package Project1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
+
 public class Main {
     public static void main(String[] args) {
         int number = 5;
         System.out.println("Factorial of " + number + " is: " + factorial(number));
+
+        String filePath = "ClassProjects/Project1/maze.dat";
+        readFile(filePath);
+    }
+
+    public static void readFile(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static int factorial(int n) {
-        System.out.println("Calculating factorial(" + n + ")");
+        // System.out.println("Calculating factorial(" + n + ")");
         if (n <= 1) {
             return 1;
         } else {
